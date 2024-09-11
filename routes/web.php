1bfa57f91f41ed2c2c7ba7060651b06db4f1c90b
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-09-11 14:35:39 +0700

    add route group /admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('/', [MainController::class, 'index'])->name('admin.index');
 });
